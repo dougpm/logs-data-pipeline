a28bafbfaf3c6314b9b40a7c83e30658cf7811b5
douglas.martins <douglas.martins@dotz.com>
2021-05-16 18:23:52 -0300

    Add Cloud Function

---
+++ cloud-functions/main.py
@@ -0,0 +1,75 @@
+from google.auth.transport.requests import Request
+from google.oauth2 import id_token
+import requests
+
+
+IAM_SCOPE = 'https://www.googleapis.com/auth/iam'
+OAUTH_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
+
+
+def trigger_dag(data, context=None):
+    """Makes a POST request to the Composer DAG Trigger API
+
+    When called via Google Cloud Functions (GCF),
+    data and context are Background function parameters.
+
+    For more info, refer to
+    https://cloud.google.com/functions/docs/writing/background#functions_background_parameters-python
+    """
+
+    client_id = '287842730866-lofdtg8pfj66aksufcu25nlla3ng3h4d.apps.googleusercontent.com'
+    tenant_project = 'q8817ba8bb0c111a9p-tp'
+    dag_name = 'process-logs'
+    webserver_url = (
+        'https://'
+        + tenant_project
+        + '.appspot.com/api/experimental/dags/'
+        + dag_name
+        + '/dag_runs'
+    )
+    # Make a POST request to IAP which then Triggers the DAG
+    make_iap_request(
+        webserver_url, client_id, method='POST', json={"conf": data, "replace_microseconds": 'false'})
+
+
+# This code is copied from
+# https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/iap/make_iap_request.py
+# START COPIED IAP CODE
+def make_iap_request(url, client_id, method='GET', **kwargs):
+    """Makes a request to an application protected by Identity-Aware Proxy.
+    Args:
+      url: The Identity-Aware Proxy-protected URL to fetch.
+      client_id: The client ID used by Identity-Aware Proxy.
+      method: The request method to use
+              ('GET', 'OPTIONS', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE')
+      **kwargs: Any of the parameters defined for the request function:
+                https://github.com/requests/requests/blob/master/requests/api.py
+                If no timeout is provided, it is set to 90 by default.
+    Returns:
+      The page body, or raises an exception if the page couldn't be retrieved.
+    """
+    # Set the default timeout, if missing
+    if 'timeout' not in kwargs:
+        kwargs['timeout'] = 90
+
+    # Obtain an OpenID Connect (OIDC) token from metadata server or using service
+    # account.
+    google_open_id_connect_token = id_token.fetch_id_token(Request(), client_id)
+
+    # Fetch the Identity-Aware Proxy-protected URL, including an
+    # Authorization header containing "Bearer " followed by a
+    # Google-issued OpenID Connect token for the service account.
+    resp = requests.request(
+        method, url,
+        headers={'Authorization': 'Bearer {}'.format(
+            google_open_id_connect_token)}, **kwargs)
+    if resp.status_code == 403:
+        raise Exception('Service account does not have permission to '
+                        'access the IAP-protected application.')
+    elif resp.status_code != 200:
+        raise Exception(
+            'Bad response from application: {!r} / {!r} / {!r}'.format(
+                resp.status_code, resp.headers, resp.text))
+    else:
+        return resp.text
+# END COPIED IAP CODE
